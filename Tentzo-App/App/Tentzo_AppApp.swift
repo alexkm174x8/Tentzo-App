f4375c2f9a9c0877ad0099bc4977f6c8d2a2b418
Alexkm174x8 <akongmontoya@hotmail.com>
2024-08-27 12:48:00 -0600

    Initial Commit

---
+++ Tentzo-App/App/Tentzo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tentzo_AppApp.swift
+//  Tentzo-App
+//
+//  Created by Administrador on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Tentzo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
