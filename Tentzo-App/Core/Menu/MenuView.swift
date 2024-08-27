a1e4ac4e7d5f7f6e8b44a071e7c70ef1f6201dcd
Alexkm174x8 <akongmontoya@hotmail.com>
2024-08-27 12:57:36 -0600

    Test 2

--- Tentzo-App/Core/Menu/MenuView.swift
+++ Tentzo-App/Core/Menu/MenuView.swift
@@ -1,8 +1,70 @@
 //
 //  MenuView.swift
-//  Tentzo-App
+//  Tentzo
 //
 //  Created by Administrador on 27/08/24.
 //
 
-import Foundation
+import SwiftUI
+import UIKit
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+                        
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
+
+struct MenuView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                VStack {
+                    HStack {
+                        Image("pp")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .padding(.leading, 20)
+                            .background(Color.blue)
+                        
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("¡Hola!, Fernanda")
+                                .font(.title)
+                                .fontWeight(.regular)
+                                .foregroundStyle(.white)
+                            
+                            HStack {
+                                Image(systemName: "location.fill")
+                                    .foregroundStyle(.white)
+                                Text("Calle 27 #142B")
+                                    .foregroundStyle(.white)
+                            }
+                            Text("Mis puntos: 114")
+                                .foregroundStyle(.white)
+                        }
+                        .padding(.leading, 10)
+                        
+                        Spacer()
+                    }
+                    .padding()
+                }
+                .background(Color(red: 83/255, green: 135/255, blue: 87/255))
+                .clipShape(RoundedCorner(radius: 25, corners: [.bottomLeft, .bottomRight]))
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuView()
+}
