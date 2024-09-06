f2a3ae160de7849e837d723da593529808d5da0f
Alexkm174x8 <akongmontoya@hotmail.com>
2024-09-06 12:16:56 -0600

    Added Nav items

--- Tentzo-App/Core/Menu/TabBar.swift
+++ Tentzo-App/Core/Menu/TabBar.swift
@@ -9,6 +9,26 @@ struct TabBar: View {
                     Image(systemName: "house")
                     Text("Home")
                 }
+            LibraryView()
+                .tabItem {
+                    Image(systemName: "leaf.fill")
+                    Text("Library")
+                }
+            CameraView()
+                .tabItem {
+                    Image(systemName: "camera.fill")
+                    Text("Camera")
+                }
+            MapView()
+                .tabItem {
+                    Image(systemName: "map.fill")
+                    Text("Map")
+                }
+            InfoView()
+                .tabItem {
+                    Image(systemName: "person.crop.circle.fill")
+                    Text("Info")
+                }
         }
     }
 }
