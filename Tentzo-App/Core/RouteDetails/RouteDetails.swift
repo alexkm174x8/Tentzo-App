a4712f415a8a3365f259d55b3395e6614adf20b9
mii1rii <mirandaarronizz@gmail.com>
2024-09-11 16:28:56 -0600

    Initial commit

---
+++ Tentzo-App/Core/RouteDetails/RouteDetails.swift
@@ -0,0 +1,27 @@
+//
+//  RouteDetails.swift
+//  Tentzo-App
+//
+//  Created by Miranda Colorado Arróniz on 11/09/24.
+//
+
+import SwiftUI
+
+struct RouteDetails: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(/*@START_MENU_TOKEN@*/.medium/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.accentColor)
+            Text("hello")
+        }
+        .padding()
+    }
+}
+
+
+struct RouteDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        RouteDetails()
+    }
+}
