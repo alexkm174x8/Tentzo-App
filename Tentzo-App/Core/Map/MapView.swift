fadb611fc3d166d405a966e52c42c86b61e6a3ff
Alexkm174x8 <akongmontoya@hotmail.com>
2024-09-12 11:49:34 -0600

    Done with Map View

--- Tentzo-App/Core/Map/MapView.swift
+++ Tentzo-App/Core/Map/MapView.swift
@@ -2,88 +2,61 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var dragOffset = CGSize.zero
-    @State private var isExpanded = false
-
+    @State private var isExpanded: Bool = false
+    
     var body: some View {
         VStack{
-            ZStack(alignment: .bottom) {
+            ZStack(alignment: .bottom){
                 Map()
-                    .edgesIgnoringSafeArea(.all)
-                
-                VStack {
-                    Spacer()
-                    BottomSheetView(isExpanded: $isExpanded)
-                        .offset(y: isExpanded ? 50 : 40)
-                        .gesture(
-                            DragGesture()
-                                .onEnded { value in
-                                    if value.translation.height < -100 {
-                                        withAnimation {
-                                            isExpanded = true
-                                        }
-                                    } else if value.translation.height > 30 {
-                                        withAnimation {
-                                            isExpanded = false
-                                        }
-                                    }
-                                    dragOffset = .zero
-                                }
-                        )
-                        .offset(y: dragOffset.height)
+                    .ignoresSafeArea(.container, edges: .top)
+                        
+                VStack{
+                    VStack {
+                        HStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                .frame(width: 35, height: 5)
+                                .foregroundStyle(Color(.lightGray))
+                                .padding(.top , 10)
+                                .opacity(0.3)
+                        }
+                        
+                        HStack(alignment: .top){
+                            Image(systemName: "map.fill")
+                                .foregroundStyle(.green)
+                            Text("Rutas")
+                                .font(.headline)
+                                .foregroundStyle(.green)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 15 )
+                        RoutePreview()
+                        RoutePreview()
+                        RoutePreview()
+                    }
+                    .frame(maxHeight: .infinity, alignment: .top) // Alinear al principio
                 }
-                
+                .frame(height: isExpanded ? 700 : 150, alignment: .top) // Alinear al principio
+                .background(.white)
+                .clipShape(RoundedCorner(radius: 25.0, corners: [.topLeft, .topRight]))
+                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: -10) // Top shadow only
+                .transition(.move(edge: .bottom))
+                .gesture(
+                    DragGesture()
+                    .onEnded { value in
+                        if value.translation.height < 50 {
+                            withAnimation {
+                                isExpanded = true
+                            }
+                        } else if value.translation.height > 30 {
+                            withAnimation {
+                                isExpanded = false
+                            }
+                        }
+                    }
+                )
             }
-        }
-    }
-}
-
-struct Map: UIViewRepresentable {
-    func makeUIView(context: Context) -> MKMapView {
-        MKMapView()
-    }
-
-    func updateUIView(_ uiView: MKMapView, context: Context) {
-        //let initialLocation = CLLocationCoordinate2D(latitude: 18.951006, longitude: -98.317351) // Ocoyucan
-        //let regionRadius: CLLocationDistance = 1000 // Radio en metros
-        //let coordinateRegion = MKCoordinateRegion(center: initialLocation, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius) uiView.setRegion(coordinateRegion, animated: true)
-    }
-}
-
-struct BottomSheetView: View {
-    @Binding var isExpanded: Bool
-
-    var body: some View {
-        VStack{
-            VStack{
-                HStack{
-                    Image(systemName: "map.fill")
-                        .foregroundStyle(.green)
-                    Text("Rutas")
-                        .font(.headline)
-                        .foregroundStyle(.green)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
-                
-                RoutePreview()
-                RoutePreview()
-                RoutePreview()
-                RoutePreview()
-                
-                
-            }
-            
-            // Agrega aquí la información que desees mostrar
-
             Spacer()
         }
-        .frame(maxWidth: .infinity)
-        .frame(height: isExpanded ? 820 : UIScreen.main.bounds.height / 3)
-        .background(Color.white)
-        .clipShape(RoundedRectangle(cornerRadius: 25))
-        .shadow(radius: 10)
-        .transition(.move(edge: .bottom))
     }
 }
 
