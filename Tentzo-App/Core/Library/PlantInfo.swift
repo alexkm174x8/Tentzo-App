f0dec9c3d196ea808d31cf92cb6dcf465240ae84
Alexkm174x8 <akongmontoya@hotmail.com>
2024-09-12 13:36:05 -0600

    Changed plant icon

--- Tentzo-App/Core/Library/PlantInfo.swift
+++ Tentzo-App/Core/Library/PlantInfo.swift
@@ -3,23 +3,19 @@ import UIKit
 
 struct PlantInfo: View {
     var body: some View {
-        Button(action: {
-                    // Acción del botón
-                }) {
-                    VStack {
-                        RoundedRectangle(cornerRadius: 30)
-                            .frame(width: 100, height: 100)
-                            .foregroundStyle(Color(red: 127/255, green: 194/255, blue: 151/255))
-                            .overlay {
-                                Image(systemName: "leaf.fill")
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 40))
-                            }
-
-                        Text("Plant")
-                            .foregroundStyle(Color.black)
-                    }
+        VStack {
+            RoundedRectangle(cornerRadius: 30)
+                .frame(width: 100, height: 100)
+                .foregroundStyle(Color(red: 127/255, green: 194/255, blue: 151/255))
+                .overlay {
+                    Image(systemName: "leaf.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 40))
                 }
+
+            Text("Plant")
+                .foregroundStyle(Color.black)
+        }
     }
 }
 
