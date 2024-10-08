14a9480b3257e74d9d50b323151a8e1054d8059a
mii1rii <mirandaarronizz@gmail.com>
2024-10-08 15:48:39 -0600

    Modified set up for Firebase, fixed

--- Tentzo-App/App/Tentzo_AppApp.swift
+++ Tentzo-App/App/Tentzo_AppApp.swift
@@ -6,21 +6,15 @@
 //
 
 import SwiftUI
-import Firebase
+import FirebaseCore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions:
-                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
-}
 
 @main
 struct Tentzo_AppApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
+    init() {
+        FirebaseApp.configure()
+    }
     @State private var isLoading = true
     @State private var isLoggedIn = false
 
